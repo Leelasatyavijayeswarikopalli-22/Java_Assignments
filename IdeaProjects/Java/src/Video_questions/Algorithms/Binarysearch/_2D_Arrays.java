c770586a4ba9da2021900a8a7fe2047a2dd3b02d
Leelasatyavijayeswarikopalli-22 <leelasatyavijayeswari1022@gmail.com>
2025-06-11 18:47:49 +0530

    modified 2D binary search

--- IdeaProjects/Java/src/Video_questions/Algorithms/Binarysearch/_2D_Arrays.java
+++ IdeaProjects/Java/src/Video_questions/Algorithms/Binarysearch/_2D_Arrays.java
@@ -4,19 +4,25 @@ import java.util.*;
 
 public class _2D_Arrays {
     public static void main(String[] args) {
-        int[][] arr={{1,2,3},{4,5,6}};
+        int[][] arr={{1,2,3},{4,5,6},{7,8,9}};
      int target=3;
         System.out.println(Arrays.toString(binary_search_2D(arr,target)));
 
     }
     static int[] binary_search_2D(int[][]arr,int target){
-        for(int i=0;i<arr.length;i++){
-            for(int j=0;j<arr[i].length;j++){
-        if(target==arr[i][j]){
-            return new int[]{i,j};
+        int r=0;
+        int c=arr.length-1;
+        while(r<arr.length&& c>=0){
+
+      if(arr[r][c]==target){
+          return new int[]{r,c};
         }
+      if(arr[r][c]<target){
+          r++;
+      }else{
+          c--;
+      }
             }
-        }
         return new int[]{-1,-1};
     }
 }
